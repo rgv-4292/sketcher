f7d56e9e77507206a29280221c04dde0dc43ab84
rvogel-CPI <r.vogel@carpentryplusinc.com>
2026-05-13 07:39:54 -0700

    12 frames

--- page.js
+++ page.js
@@ -502,8 +502,8 @@ export class Page {
   }
 
   async startTransition(newJSON) {
-    const FRAMES = 24
-    const FRAME_DURATION = 42
+    const FRAMES = 12
+    const FRAME_DURATION = 83
 
     const fromMarks = this.marks.map(m => Mark.fromJSON(m.toJSON()))
     const toMarks = newJSON.marks.map(markData => Mark.fromJSON(markData))

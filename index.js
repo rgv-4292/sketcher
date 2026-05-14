0f2508e5f00c5a908b86a048c21d48f6e2098014
rvogel-CPI <r.vogel@carpentryplusinc.com>
2026-05-14 14:00:29 -0700

    gdfg

--- index.js
+++ index.js
@@ -196,7 +196,7 @@ document.addEventListener('DOMContentLoaded', function () {
   function placeImport(tapX, tapY) {
     const offsetX = tapX - importCentroid.x
     const offsetY = tapY - importCentroid.y
-    // const rotated = rotateMarks(importMarks, importRotationDeg)
+    // const rotated = rotateMarks(importMarks, importRotationDeg)  // this is unnecessary
     const rotated = importMarks
 
 

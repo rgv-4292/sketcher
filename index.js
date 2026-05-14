4a5aa164cbce50229424b9eadb8d62a3fe7f97fb
rvogel-CPI <r.vogel@carpentryplusinc.com>
2026-05-14 14:10:50 -0700

    gkgk

--- index.js
+++ index.js
@@ -137,8 +137,8 @@ document.addEventListener('DOMContentLoaded', function () {
     if (importGhostBitmap) { importGhostBitmap.bitmap.close(); importGhostBitmap = null }
 
     // Bake ghost with the current importRotationDeg applied
-    const toRotate = importRotationDeg !== 0 ? rotateMarks(importMarks, importRotationDeg) : importMarks
-    // const toRotate = importMarks
+    // const toRotate = importRotationDeg !== 0 ? rotateMarks(importMarks, importRotationDeg) : importMarks
+    const toRotate = importMarks
     const forBake = toRotate.map(m => {
       const clone = Mark.fromJSON(m.toJSON())
       clone.points = m.points.map(p => ({ ...p, x: p.x + importCentroid.x, y: p.y + importCentroid.y }))
